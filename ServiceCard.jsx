a93e690bf325925125eb6fc834bb0508bb1f9081
Arshith-a-k <arshith.baala@kalvium.community>
2025-04-23 13:02:27 +0530

    Add files via upload

---
+++ ServiceCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ServiceCard({service_title,service_description}){
+    return(
+        <div>
+            <p>{service_title}</p>
+            <p>{service_description}</p>
+        
+        </div>
+    )
+}
+
+export default ServiceCard;
